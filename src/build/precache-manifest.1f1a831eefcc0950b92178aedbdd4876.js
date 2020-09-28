46adcb54dda0e3a6c3f4793b900e22d6bce6c199
avoitenco <avoitenco@outlook.com>
2020-09-28 17:03:21 -0600

    Second version

---
+++ src/build/precache-manifest.1f1a831eefcc0950b92178aedbdd4876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097851fa4293e7f524db65fd4268b21d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fb2a05c4135af98df7",
+    "url": "/static/css/main.f8b6f820.chunk.css"
+  },
+  {
+    "revision": "d7c7ef62f562b707ab01",
+    "url": "/static/js/2.e7e1d73f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e7e1d73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60fb2a05c4135af98df7",
+    "url": "/static/js/main.27c51e78.chunk.js"
+  },
+  {
+    "revision": "35c6bc6620fd0e11f636",
+    "url": "/static/js/runtime-main.0ed6e50a.js"
+  }
+]);
